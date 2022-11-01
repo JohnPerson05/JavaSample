22e33db5c2eb90777cf54f80c0ea734a88930395
johnperson_narral <john.person.narral@accenture.com>
2022-11-01 19:43:16 +0800

    Second commit

--- src/learnJava_sample/HelloWorld.java
+++ src/learnJava_sample/HelloWorld.java
@@ -22,6 +22,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("asd");
+		System.out.println("Hello worlds");
 
 	}
 
